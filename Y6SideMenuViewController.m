c9ef65d922acee3521db697df226734be2ec7b0a
Ysix <y-6@hotmail.fr>
2014-08-27 12:37:29 +0200

    renaming methods fix

--- Y6SideMenuViewController.m
+++ Y6SideMenuViewController.m
@@ -129,7 +129,7 @@
     }
 }
 
-- (void)sidemenuDidOpen
+- (void)sideMenuDidOpen
 {
 
     if (dimissSideMenuView.hidden)
@@ -140,7 +140,7 @@
 
 }
 
-- (void)sidemenuDidClose
+- (void)sideMenuDidClose
 {
 	[dimissSideMenuView setHidden:YES];
 }
@@ -235,13 +235,13 @@
         if (scrollView.contentOffset.x == GLOBALSV_MENU_OPENED_OFFSET.x)
         {
             [self.view bringSubviewToFront:sideMenuView];
-            [self sidemenuDidClose];
+            [self sideMenuDidClose];
         }
         else
             [self.view sendSubviewToBack:sideMenuView];
 
         if (scrollView.contentOffset.x == GLOBALSV_MENU_CLOSED_OFFSET.x)
-            [self sidemenuDidOpen];
+            [self sideMenuDidOpen];
     }
 }
 
