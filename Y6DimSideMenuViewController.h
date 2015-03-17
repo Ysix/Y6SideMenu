3940c5fd3026c361cedb3e71c1098f5e3cd9b267
Ysix <y-6@hotmail.fr>
2015-03-17 16:09:38 +0100

    Add Dim Alternative

---
+++ Y6DimSideMenuViewController.h
@@ -0,0 +1,18 @@
+//
+//  Y6DimSideMenuViewController.h
+//  rad
+//
+//  Created by Eddy Claessens on 01/01/2015.
+//  Copyright (c) 2015 ysixapps. All rights reserved.
+//
+
+#import "Y6SideMenuViewController.h"
+
+#define MAX_DIM 0.8
+
+@interface Y6DimSideMenuViewController : Y6SideMenuViewController
+{
+    UIView *dimmedView;
+}
+
+@end
